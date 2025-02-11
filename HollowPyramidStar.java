95666cd79b3b3eb0b8ba56660cc93251c0d41a2a
MAHANANDHA J <124136528+Mahanandha10@users.noreply.github.com>
2025-02-11 09:35:54 +0530

    Add files via upload

---
+++ HollowPyramidStar.java
@@ -0,0 +1,25 @@
+public class HollowPyramidStar {
+    public static void main(String[] args) {
+        int size =5;
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size -i-1;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<2*i+1;k++){
+                
+                if(i==0||i==size-1){
+                    System.out.print("*");
+                }
+                else{
+                    if(k==0||k==2*i){
+                    System.out.print("*");
+                }
+                else{
+                    System.err.print(" ");
+                }
+            }
+        }
+            System.out.println();
+        }
+    }
+}
