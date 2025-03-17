abfe61694ddf0d7784928689818173a3f4302e9a
MAHANANDHA J <124136528+Mahanandha10@users.noreply.github.com>
2025-03-17 08:06:19 +0530

    Add files via upload

---
+++ ReverseLinkedList.java
@@ -0,0 +1,17 @@
+class ReverseLinkedList{
+    public static boolean bat(String s){
+        int left=0;
+        int right = s.length() -1;
+        while (left<right) {
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(bat("mad"));
+    }
+}
